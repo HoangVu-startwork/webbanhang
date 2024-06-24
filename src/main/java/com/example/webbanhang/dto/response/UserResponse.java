8c552eafa0d84d8074994870b4af8e86dc20b47a
HoangVu-startwork <hoangvu.startwork@gmail.com>
2024-06-25 01:00:45 +0700

    Phân quyền (Authorization) bằng JWT

--- src/main/java/com/example/webbanhang/dto/response/UserResponse.java
+++ src/main/java/com/example/webbanhang/dto/response/UserResponse.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,11 +13,11 @@ import lombok.NoArgsConstructor;
 public class UserResponse {
     private String id;
     private String username;
-    private String password;
     private String email;
     private String firsName;
     private String lastName;
     private String phone;
     private String ngaysinh;
     private String dob;
+    private Set<String> roles;
 }
---
+++ src/main/java/com/example/webbanhang/enums/Role.java
@@ -0,0 +1,6 @@
+package com.example.webbanhang.enums;
+
+public enum Role {
+    ADMIN,
+    USER
+}
