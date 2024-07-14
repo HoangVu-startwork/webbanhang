95f63cc50efdcca05c50305ed428d18b7b03de23
HoangVu-startwork <hoangvu.startwork@gmail.com>
2024-07-14 22:05:42 +0700

    them

---
+++ src/main/java/com/example/webbanhang/dto/request/DienthoaiRequest.java
@@ -0,0 +1,19 @@
+package com.example.webbanhang.dto.request;
+
+import lombok.*;
+
+@Setter
+@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DienthoaiRequest {
+    private String tensanpham;
+    private String hinhanh;
+    private String hinhanhduyet;
+    private String ram;
+    private String bonho;
+    private String giaban;
+    private String tenphanloai;
+}
